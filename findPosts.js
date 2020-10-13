f8618b335df9c92c45e0246be907c0eb7e80e277
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-10-12 21:27:25 -0400

    comment event listener working

--- findPosts.js
+++ findPosts.js
@@ -1,28 +1,3 @@
-/*
-
-Build a web application that shows a list of posts and information about the post's author. This will require the following steps:
-
-API endpoint: https://jsonplaceholder.typicode.com/
-
-1. Make an API call to retrieve all of the posts.
-2. Make an API call to retrieve all of the users.
-3. Display the author's name and the post title and body on the page.
-
-Grading criteria:
-1. Use fetch, async, and await.
-2. Use the find() method.
-3. Use only ES6+ techniques if needed: template strings (`${}`), arrow functions, forEach(), etc.
-4. Use GitHub and make at least 5 commits with meaningful commit messages in the present tense. For example: "Create filter function."
-5. Do not have any extraneous comments beyond explanations of code (if needed) in your final result. Do not have any console.logs, in comments or otherwise.
-6. CSS is not necessary and will not be graded but is encouraged for the sake of practice.
-
-Stretch goal:
-
-When a user clicks on a post, make an API call to retrieve the comments associated with that
-post and display them on the page.
-
-*/
-
 const getPosts = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const posts = await response.json();
@@ -37,24 +12,20 @@ const getUsers = async () => {
 
 };
 
-//`"https://jsonplaceholder.typicode.com/comments?postId=${""}`
 const getComments = async (posts) => {
     const response3 = await fetch("https://jsonplaceholder.typicode.com/comments");
     const postComments = await response3.json();
     return postComments;
 
-};  // new stretch
+};
 
-let commentButton;
-let blogEntry;
-
-const showBlogPosts = async (posts, users, comments) => {    // new stretch added comments arg
+const showBlogPosts = async (posts, users, comments) => {
     const result = document.getElementsByClassName("result")[0];
     const postTitle = await getPosts();
-    const commentsOnPost = await getComments();   // new stretch
+    const commentsOnPost = await getComments();
     const usersName = await getUsers();
     postTitle.forEach((posts, e) => {
-        blogEntry = document.createElement("div");
+        let blogEntry = document.createElement("div");
         blogEntry.classList.add("blogEntry");
         blogEntry.style.display = "block";
         result.appendChild(blogEntry);
@@ -65,43 +36,28 @@ const showBlogPosts = async (posts, users, comments) => {    // new stretch adde
             };
         });
         blogEntry.innerHTML += `<div class="postBody">${posts.body}</div>`;
+
+        let commentButton = document.createElement("div");
+        commentButton.classList.add("commentButton");
+        commentButton.classList.add("show");
+        commentButton.innerHTML = "Comments";
+        blogEntry.appendChild(commentButton);
+        commentButton.addEventListener("click", () => {
+            commentBox.classList.toggle("show");
+        });
         let commentBox = document.createElement("div");
         commentBox.classList.add("commentBox");
         commentBox.classList.add("hide");
         blogEntry.appendChild(commentBox);
-        commentButton = blogEntry.innerHTML += `<div class="postComments">Comments</div></br>`;
+
         commentsOnPost.filter((comments) => {
             if(comments.postId === posts.id){
                 commentBox.innerHTML += `<div class="postCommentsName">${comments.name}</div></br>
                                         <div class="postCommentsBody">${comments.body}</div></br>`;
             };
         });
-        let commentClickers = document.querySelectorAll('.hide');
-        commentClickers.forEach(commentBox => {
-            commentBox.addEventListener("click", () => {
-                commentBox.classList.toggle("show");
-            });
-        });
     });
-    // commentButton.addEventListener("click", async() => {
-    //     commentBox = document.getElementsByClassName("commentBox")[0];
-    //     if (commentBox.style.display === "none") {
-    //         commentBox.style.display = "block";
-    //       } else {
-    //         commentBox.style.display = "none";
-    //       }
-    // });
-
-
 };
-
-// let clickable = commentButton.addEventListener("click", () => {
-//     if (commentBox.style.display === "none") {
-//         commentBox.style.display = "block";
-//       } else {
-//         commentBox.style.display = "none";
-//       }
-// });
 
 showBlogPosts()
 
